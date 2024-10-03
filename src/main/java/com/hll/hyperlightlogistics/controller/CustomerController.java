b9e2ea27b1fcec4a89c4760c0b1754fabe3234f2
ADefaultDev <im7ckngmad@gmail.com>
2024-10-03 20:09:57 +0500

    Pre-configured service and controller classes for Customer

---
+++ src/main/java/com/hll/hyperlightlogistics/controller/CustomerController.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of HyperLightLogistics-Java.
+ *
+ * HyperLightLogistics-Java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HyperLightLogistics-Java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HyperLightLogistics-Java.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2024 Vsevolod Batyrov
+ */
+
+package com.hll.hyperlightlogistics.controller;
+
+import com.hll.hyperlightlogistics.model.Customer;
+import com.hll.hyperlightlogistics.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping("/addCustomer")
+    public ResponseEntity<Customer> createCustomer() {
+
+        Customer customer = null;
+        return ResponseEntity.ok(customer);
+    }
+}
