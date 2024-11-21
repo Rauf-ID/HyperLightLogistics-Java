1a42661f137534eb4719c72abfea72ba05273044
Rauf-ID <rauf.develop@gmail.com>
2024-11-21 22:34:34 +0500

    Added transport models for generating tables in the database. Used in the service HyperLightLogistics-Go

---
+++ src/main/java/com/hll/hyperlightlogistics/model/transports/Truck.java
@@ -0,0 +1,58 @@
+/*
+ * This file is part of HyperLightLogistics-Java.
+ *
+ * HyperLightLogistics-Java is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * HyperLightLogistics-Java is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with HyperLightLogistics-Java.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2024 Rauf Agaguliev
+ */
+
+package com.hll.hyperlightlogistics.model.transports;
+
+import com.hll.hyperlightlogistics.model.Warehouse;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "trucks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Truck {
+
+    @Id
+    @SequenceGenerator(name = "trucks_seq", sequenceName = "trucks_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "trucks_seq")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "warehouse_id")
+    private Warehouse warehouse;
+
+    private String status;
+    private Float capacity;
+    private Float load;
+    private LocalDateTime nextAvailableTime;
+
+}
